0c157b0fa188642b7bce2a24e2b62b868e21eaea
VeryGoodNaeme <127005284+VeryGoodNaeme@users.noreply.github.com>
2023-05-16 15:18:49 -0400

    Made DWM status bar look more minimal

--- dotdwm/autostart.sh
+++ dotdwm/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while true; do
-	setroot -name "[  $(amixer -D pipewire get Master | awk -F 'Left:|[][]' 'BEGIN {RS=""}{ print $3 }') ][  $(date +"%B %d %Y") ][  $(date +"%I:%M %p") ]"
-        sleep 3s
+	xsetroot -name xsetroot -name "$(date +"%B %d %I:%M %p")"
+        sleep 1m
 done &
