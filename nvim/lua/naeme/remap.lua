501cb39d3322627af23c13db8fbfd31719ed71a4
Very Good Naeme <127005284+VeryGoodNaeme@users.noreply.github.com>
2023-10-18 15:50:35 -0400

    added split keybinds to nvim

--- nvim/lua/naeme/remap.lua
+++ nvim/lua/naeme/remap.lua
@@ -2,3 +2,7 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+--splits
+vim.keymap.set("n", "<leader>v", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>h", vim.cmd.split)
+
